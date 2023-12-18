7ff0cedf5027fedba3e99beef89787fdcdb06e34
katieperlitz1 <142810960+katieperlitz1@users.noreply.github.com>
2023-12-18 01:16:42 +0000

    lots o stuff

---
+++ scripts/button.js
@@ -0,0 +1,8 @@
+const whitebutton = document.querySelector('.whitebutton')
+const whitebuttontext = document.querySelector('.whitea')
+
+whitebutton.addEventListener('mouseover', () => {
+    whitebuttontext.style.color = 'white';
+});
+
+
